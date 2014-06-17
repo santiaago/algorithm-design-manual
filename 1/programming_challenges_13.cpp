c38d285e3e73c4a03e83820e900389b45223d315
santiaago <santiago.ariassar@gmail.com>
2014-06-17 17:36:38 -0500

    the australian vote with map record

--- 1/programming_challenges_13.cpp
+++ 1/programming_challenges_13.cpp
@@ -5,11 +5,13 @@
 #include <vector>
 #include <sstream>
 #include <string>
+#include <map>
 
 using namespace std;
 
 template<typename T>
 void printVector(vector<T> v);
+void printMap(map<string, unsigned int> m);
 
 int main(){
   cout << "Australian Voting" << endl;
@@ -21,13 +23,17 @@ int main(){
     unsigned int num_candidates(0);
     cin >> num_candidates;
     vector<string> candidates(num_candidates);
+    map<unsigned int, map<string, unsigned int> > candidate_votes;
+
     string candidate;
     while( num_candidates > 0 && cin >> candidate ){
       candidates[candidates.size()-num_candidates] = candidate;
       num_candidates--;
     }
     printVector(candidates);
+    //printMap(candidate_votes);
     // read up to 1000 lines
+    
     string vote;
     unsigned int num_votes(0);
     cin.ignore();
@@ -40,8 +46,23 @@ int main(){
       while( stm >> index_candidate){
 	current_vote.push_back(index_candidate);
       }
+      for(int i = 0; i < current_vote.size(); i++){
+      	//candidate_votes[candidates[current_vote[i] - 1]]++;
+	cout << "vote: " << current_vote[i] << endl;
+	cout << "candidate: " << candidates[current_vote[i]-1] << endl;
+	cout << "count: " << candidate_votes[i + 1][candidates[current_vote[i]-1]] << endl;
+	candidate_votes[i + 1][candidates[current_vote[i]-1]]++;//candidates[current_vote[i]][
+	cout << "count: " << candidate_votes[i + 1][candidates[current_vote[i]-1]] << endl;
+      }
       printVector(current_vote);
       num_votes++;
+    }
+    // printMap(candidate_votes);
+    for(auto t : candidate_votes){
+      cout << t.first << " | ";
+      for(auto t2 : t.second){
+	cout << "\t" << t2.first << " : " << t2.second << endl;
+      } 
     }
     cout << "case done " << endl;
     num_cases--;
@@ -55,4 +76,10 @@ void printVector(vector<T> v){
     cout << c << " | ";
   }
   cout << endl;
+}
+
+void printMap(map<string, unsigned int> m){
+  for(auto t : m){
+    cout << t.first << " : " << t.second << endl;
+  }
 }
