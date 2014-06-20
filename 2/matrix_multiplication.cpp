4c5ef24a39dd71233fc9b13372ed89435c01c683
santiaago <santiago.ariassar@gmail.com>
2014-06-20 12:05:15 -0500

    first take at matrix multiplication

---
+++ 2/matrix_multiplication.cpp
@@ -0,0 +1,63 @@
+//  matrix_multiplication.cpp : santiaago
+//  Description: matrix multiplication algorithm
+
+#include<iostream>
+
+using namespace std;
+
+void printMatrix(int** a, int size);
+int** matrix_multiplication(int** a, int** b, int size);
+
+int main(){
+  cout << "matrix multiplication" << endl;
+  int **a;
+  int **b;
+  int size = 3;
+  a = new int *[size];
+  b = new int *[size];
+
+  for(int i = 0; i < size; i++){
+    a[i] = new int [size];
+    b[i] = new int [size];
+  }
+
+  for(int i = 0; i < size; i++){
+    for(int j = 0; j < size; j++){
+      a[i][j] = 1;
+      b[i][j] = 2;
+    }
+  }
+  printMatrix(a, size);
+  cout << endl;
+  printMatrix(b, size);
+  cout << endl;
+  int ** c = matrix_multiplication(a, b, size);
+  printMatrix(c, size);
+}
+
+void printMatrix(int** a, int size){
+  for(int i = 0; i < size; i++){
+    for(int j = 0; j < size; j++){
+      cout << a[i][j] << " ";
+    }
+    cout << endl;
+  }
+}
+
+int** matrix_multiplication(int** a, int** b, int size){
+  int **c;
+  c = new int *[size];
+  for(int i = 0; i < size; i++){
+    c[i] = new int[size];
+  }
+  for(int i = 0; i < size; i++){
+    for(int j = 0; j < size; j++){
+      int sum(0);
+      for(int k = 0; k < size; k++){
+	sum += a[i][j]*b[k][j];
+      }
+      c[i][j] = sum;
+    }
+  }
+  return c;
+}
