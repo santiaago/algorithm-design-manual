6a0c4ee980f5f71a5fd9ff4418e05315a743c6ae
santiaago <santiago.ariassar@gmail.com>
2014-06-26 09:46:30 -0500

    change algorithm for square pattern as simple one was TLE <=> time limit exceeded

--- 2/programming_challenges_23.cpp
+++ 2/programming_challenges_23.cpp
@@ -3,26 +3,27 @@
 
 #include <iostream>
 #include <vector>
+#include <stdlib.h>
+#include <string>
+#include <math.h>
 
 using namespace std;
 
 int main(){
-  cout << "Light, More Light" << endl;
+  //cout << "Light, More Light" << endl;
 
   vector<bool> lights;
   string line;
   while(getline(cin, line, '\n')){
 
-    long current_number = atol(line.c_str());
+    long long current_number = atol(line.c_str());
     if(current_number == 0) break;
-    
-    bool on(false);
-    for(long i = 1; i <= current_number; i++){
-      if((current_number%i) == 0){
-	on = on? false: true;
-      }
+    long long root = sqrt(current_number);
+    if((root * root) == current_number){
+      lights.push_back(true);
+    } else{
+      lights.push_back(false);
     }
-    lights.push_back(on);
   }
   for(int i = 0; i < lights.size(); i++){
     if(lights[i]){
